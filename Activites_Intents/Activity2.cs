76f1e50c3fb89af4060c03be3cae3b7eb3d72545
xamarin_2355 <xamarin_2355@NTID60-2355-07>
2021-09-10 12:03:00 -0400

    Add project files.

---
+++ Activites_Intents/Activity2.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Activites_Intents
+{
+    [Activity(Label = "Activity2")]
+    public class Activity2 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_2);
+            TextView textView = FindViewById<TextView>(Resource.Id.myTextView);
+
+            var message = base.Intent.GetStringExtra("secretLoveMessage");
+            textView.Text = message;
+        }
+    }
+}
---
+++ Activites_Intents/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace Activites_Intents
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int count = 0;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            //button.Click += (o, e) => {
+            //  Toast.MakeText(this, "Beep Boop", ToastLength.Short).Show();
+            //};
+            button.Click += myButton_Click;
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            //count++;
+            //Toast.MakeText(this, "Beep Boop " + count.ToString(), ToastLength.Short).Show();
+            var bundle = new Bundle();
+            bundle.PutString("secretLoveMessage", "You're so hot!!");
+
+            var intent = new Intent(this, typeof(Activity2));
+            intent.PutExtras(bundle);
+            base.StartActivity(intent);
+        }
+
+    }
+}
